a32db6c5b25764b5be3b1aa6a4f6e68356291685
Augie Bren <Augie>
2022-06-14 01:38:41 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import React, { useState } from 'react'
+import ColorBlock from './component/ColorBlock';
+import ColorForm from './component/ColorForm';
 
 function App() {
+  let [colors, setColors] = useState(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+  
+  const colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} />
+    )
+  })
+
+  
+
   return (
     <div className="App">
-      
+      {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
